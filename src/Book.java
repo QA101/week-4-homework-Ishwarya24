67f60dd06211919bdf9b1faa7b69365cb053500e
Dustin <dpaulkinney@gmail.com>
2017-11-06 20:32:03 -0800

    Restructured to use methods

--- src/Book.java
+++ src/Book.java
@@ -3,16 +3,22 @@ public class Book{
 	String title;
 	int pages;
 	String category;
+	String summary;
 	
 	
-	public Book(String title, int pages, String category) {
+	public Book(String title, int pages, String category, String summary) {
 		this.title = title;
 		this.pages = pages;
 		this.category = category;
+		this.summary = summary;
 	}
 	
 	public String toString() {
 		return title;
+	}
+	
+	public String Details() {
+		return "Title: " + title + ", #pages: " + pages + ", Summary: " + summary;
 	}
 	
 	public String getTitle() {
