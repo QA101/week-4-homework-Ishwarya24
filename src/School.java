928685532a4596453d25a4c95c1c47ef220a7ae4
Dustin <dpaulkinney@gmail.com>
2017-11-08 21:20:21 -0800

    Upadted instructions for school

--- src/School.java
+++ src/School.java
@@ -1,30 +1,32 @@
 import java.util.ArrayList;
 
-
-/**
- * This is a school object. It is meant to mimic a real school.
- * 
- * Schools have books, students, teachers, principles, classes.
- * 
- * For this homework, create a school that has a list of books. Print that list.
- * 
- * Create other methods that would make sense for a school. Ideas:
- * Add students, remove students, add teachers, open the school, close the school.
- * 
- * Be creative.
- * 
- * CHALLENGE: Can a school have a library?
- * 
- * @author dustin
- *
- */
 public class School {
 	
 	//Variables for this class
 	ArrayList<Book> catalog = new ArrayList<Book>();
 	int numStudents = 0;
 	String name;
-	
+
+	/**
+	 * Start the execution of your program here.
+	 * 
+	 * --------------INSTRUCTIONS----------------
+	 * 1. The main method should create a school, add books to its catalog, and print all the books in its catalog.
+	 * Use the library class for reference.
+	 * 
+	 * 2. After printing the books, the program should add students and print the total number of student.
+	 * 
+	 * 3. Finally, the program should call other methods that you design and print anything that makes sense for a school.
+	 * 
+	 * Thoughts:
+	 * Schools have books, students, teachers, principles, classes.
+	 * Add students, remove students, add teachers, open the school, close the school.
+	 * 
+	 * ----------------CHALLENGE:----------------
+	 * 
+	 * Can a school have a library? How would this be represented within the school?
+	 * 
+	 */
 	public static void main(String args[]) {
 		//TODO create a school (similar to the library)
 		
@@ -50,16 +52,29 @@ public class School {
 		return "TODO";
 	}
 	
-	//adds a single student to the school
+	/**
+	 * adds a single student to the school
+	 */
 	public void AddSingleStudent() {
 		numStudents++;
 		//Do I need a MAX_STUDENTS ???
 	}
 	
-	//removes a single student from the school
+	/**
+	 * removes a single student from the school
+	 */
 	public void removeSingleStudent() {
 		if(numStudents > 0) {
 			numStudents--;
 		}
+	}
+	
+	/**
+	 * method to return number of students
+	 * 
+	 * @return number of students in the school
+	 */
+	public int getStudents() {
+		return numStudents;
 	}
 }
